3f8a83b92c61f5ab622b2fc96f7063291ed965dc
YatharthMittal13 <mittalyatharth132004@gmail.com>
2024-09-15 20:09:49 +0530

    added cart page, auth-register and login page

--- server.js
+++ server.js
@@ -2,24 +2,42 @@ const express = require('express');
 const app = express();
 
 const ejs = require('ejs');
-const expresslayout = require('express-ejs-layouts');
+const expressLayout = require('express-ejs-layouts');  //this package is used to avoid multiple repeatation of same thing(nav) in different pages 
 
 const path = require('path');
 
-//assests
+//assestss
 app.use(express.static('public'));
 
 
+//set template engine
+app.use(expressLayout);
+app.set('views' , path.join(__dirname , '/resources/views/'));
+app.set('view engine', 'ejs');
 
-app.get('/', (req , resp)=>{
+
+
+app.get('/', (req , resp)=>{ //for home page
     resp.render('home')     //go to views folder and then home.ejs
 });                          //template engine is providing locationof home.ejs
 
 
-//set template engine
-app.use(expresslayout);
-app.set('views' , path.join(__dirname , '/resources/views'));
-app.set('view engine', 'ejs');
+//for cart page
+app.get('/cart', (req,resp)=>{
+    resp.render('customers/cart')
+})
+
+//for login page
+app.get('/login', (req,resp)=>{
+    resp.render('auth/login')
+})
+
+
+//for register page
+app.get('/register', (req,resp)=>{
+    resp.render('auth/register')
+})
+
 
 
 const PORT = process.env.PORT || 3300
